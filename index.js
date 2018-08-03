75face00461dc853bf8b97348a15d7079c41d70c
Alex S <alex.sunga@fliptech.co>
2018-08-03 06:58:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,24 +3,13 @@ function takeANumber (currentLine, name){
     return `Welcome, ${name}. You are number ${currentLine.length} in line.`
 }
 
-function currentLine(currentLine){
-  if(!currentLine.length){
-    return "The line is currently empty"
+function nowServing (currentLine){
+  if (!currentLine.length){
+    return "There is nobody waitng to be served!"
   }
+    return `Currently serving ${currentLine.shift()}.`
 }
-  const lineNumberName = []
-  for (let i = 0, l = currentLine.length; i < l; i++)
-function currentLine(line) {
-if (!line.length) {
-return "The line is currently empty."
-}
-const numbersAndNames = []
-for (let i = 0, l = line.length; i < l; i++) {
-numbersAndNames.push(`${i + 1}. ${line[i]}`)
-}
-return `The line is currently: ${numbersAndNames.join(', ')}`
 
-}
 
 
 
