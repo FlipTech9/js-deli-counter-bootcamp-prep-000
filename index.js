033bdf73222483b419cc870245eaf5fc776cd0b8
Alex S <alex.sunga@fliptech.co>
2018-08-03 07:05:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function takeANumber (line, name){
-    line.push(name)
+function takeANumber (currentLine, name){
+    currentLine.push(name)
     return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
