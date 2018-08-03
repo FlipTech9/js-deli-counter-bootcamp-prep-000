aa7157c393477cb7db02879a2a8e42561b74d84d
Alex S <alex.sunga@fliptech.co>
2018-08-03 06:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ function takeANumber (currentLine, name){
     return `Welcome, ${name}. You are number ${currentLine.length} in line.`
 }
 
+function currentLine(currentLine){
+  if (!currentLine.length){
+    return "The line is currently empty."
+  }
+  const lineNumberName = []
+  for (let i = 0; i < currentLine.length; i++){
+    lineNumberName.push(`${i+1}. ${currentLine[i]}`)
+  }
+  return `The line is currently: ${lineNumberName.join(',  ')}`
+}
 
 
 
