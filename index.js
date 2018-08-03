aa22c735e4df89480e909bc881cb256a5f41acd8
Alex S <alex.sunga@fliptech.co>
2018-08-03 07:09:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,21 @@ function currentLine(currentLine){
   }
   const lineNumberName = []
   for (let i = 0, l = currentLine.length; i < l; i++){
-    lineNumberName.push(`${i+l}. ${currentLine[i]}`)
+    lineNumberName.push(`${i + 1}. ${currentLine[i]}`)
   }
-  return `The line is currently: ${lineNumberName.join(',  ')}`
+  return `The line is currently: ${lineNumberName.join(', ')}`
 }
 
+function currentLine(line) {
+  if (!line.length) {
+  return "The line is currently empty."
+  }
+  const numbersAndNames = []
+  for (let i = 0, l = line.length; i < l; i++) {
+  numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+}
 
 
 
