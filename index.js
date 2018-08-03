01244e070b7cf0c78731733397fc3d6dab81a621
Alex S <alex.sunga@fliptech.co>
2018-08-03 06:42:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function takeANumber (currentLine, name){
     return `Welcome, ${name}. You are number ${currentLine.length} in line.`
 }
 
-function nowServing (currentLine){
-  if (!currentLine.length){
+function nowServing (line){
+  if (!line.length){
     return "There is nobody waitng to be served!"
   }
     return `Currently serving ${currentLine.shift()}.`
